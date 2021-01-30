96f5429e8053a959cf761a6c320321f563667b9d
milton.bastos <milton.bastos@ambevtech.com.br>
2021-01-29 21:17:04 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: white;
+  }
+`
+
+const theme = db.theme;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
